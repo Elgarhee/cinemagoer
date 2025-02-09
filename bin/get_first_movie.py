89d47c89448b6401fc9259892e6ab1720fd95b63
Amr El Garhee <41242368+Elgarhee@users.noreply.github.com>
2025-02-10 00:28:40 +0200

    Update get_first_movie.py

--- bin/get_first_movie.py
+++ bin/get_first_movie.py
@@ -9,6 +9,7 @@ Search for the given title and print the best matching result.
 """
 
 import sys
+from Levenshtein import distance as levenshtein_distance
 
 # Import the Cinemagoer package.
 try:
@@ -23,7 +24,7 @@ if len(sys.argv) != 2:
     print('  %s "movie title"' % sys.argv[0])
     sys.exit(2)
 
-title = sys.argv[1]
+title = sys.argv[1].lower()
 
 
 i = imdb.IMDb()
@@ -40,11 +41,22 @@ if not results:
     print('No matches for "%s", sorry.' % title)
     sys.exit(0)
 
-# Print only the first result.
+# Filter results to include only movies
+movie_results = [result for result in results if result.get('kind') == 'movie']
+
+if not movie_results:
+    print('No movie matches for "%s", sorry.' % title)
+    sys.exit(0)
+
+# Choose the best match from the top two results based on Levenshtein distance
+top_two_results = movie_results[:2]
+best_match = min(top_two_results, key=lambda movie: levenshtein_distance(title, movie.get('title', '').lower()))
+
+# Print the best result
 print('    Best match for "%s"' % title)
 
 # This is a Movie instance.
-movie = results[0]
+movie = best_match
 
 # So far the Movie object only contains basic information like the
 # title and the year; retrieve main information:
