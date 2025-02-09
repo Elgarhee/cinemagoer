a0b4b9d251407f0cb99d26adce1d1a1bbc1b8e1d
Amr El Garhee <41242368+Elgarhee@users.noreply.github.com>
2025-02-10 00:30:26 +0200

    Update search_movie.py

--- bin/search_movie.py
+++ bin/search_movie.py
@@ -5,10 +5,11 @@ search_movie.py
 
 Usage: search_movie "movie title"
 
-Search for the given title and print the results.
+Search for the given title and print the best matching result.
 """
 
 import sys
+from Levenshtein import distance as levenshtein_distance
 
 # Import the Cinemagoer package.
 try:
@@ -23,8 +24,7 @@ if len(sys.argv) != 2:
     print('  %s "movie title"' % sys.argv[0])
     sys.exit(2)
 
-title = sys.argv[1]
-
+title = sys.argv[1].lower()
 
 i = imdb.IMDb()
 
@@ -38,14 +38,33 @@ except imdb.IMDbError as e:
     print(e)
     sys.exit(3)
 
-# Print the results.
-print('    %s result%s for "%s":' % (len(results),
-                                     ('', 's')[len(results) != 1],
-                                     title))
-print('movieID\t: imdbID : title')
+if not results:
+    print('No matches for "%s", sorry.' % title)
+    sys.exit(0)
 
-# Print the long imdb title for every movie.
-for movie in results:
-    outp = '%s\t: %s : %s' % (movie.movieID, i.get_imdbID(movie),
-                               movie['long imdb title'])
-    print(outp)
+# Filter results to include only movies
+movie_results = [result for result in results if result.get('kind') == 'movie']
+
+if not movie_results:
+    print('No movie matches for "%s", sorry.' % title)
+    sys.exit(0)
+
+# Choose the best match from the top two results based on Levenshtein distance
+top_two_results = movie_results[:2]
+best_match = min(top_two_results, key=lambda movie: levenshtein_distance(title, movie.get('title', '').lower()))
+
+# Print the best result
+print('    Best match for "%s":' % title)
+print('movieID\t: imdbID : title')
+outp = '%s\t: %s : %s' % (best_match.movieID, i.get_imdbID(best_match),
+                          best_match['long imdb title'])
+print(outp)
+
+# This is a Movie instance.
+movie = best_match
+
+# So far the Movie object only contains basic information like the
+# title and the year; retrieve main information:
+i.update(movie)
+
+print(movie.summary())
